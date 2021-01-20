e885d11cca07f496a70510567c7941fbccd55199
chenlong_cxy <chenlonghyl@163.com>
2021-01-20 20:01:26 +0800

    daily code

---
+++ test_2021_1_20/test_2021_1_20/test.c
@@ -0,0 +1,47 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	int n = 10;
+//	while (n)
+//	{
+//		int ret = rand()%10;
+//		printf("%d ", ret);
+//		n--;
+//	}
+//	return 0;
+//}
+//RAND_MAX
+//0x7fff
+//32767
+//时间戳：当前时间与计算机起始时间的差值，单位是秒
+//计算机起始时间：1970年1月1日8点
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//int main()
+//{
+//	int n = 10;
+//	srand((unsigned int)time(NULL));
+//	while (n)
+//	{
+//		int ret = rand()%900+100;
+//		printf("%d\n", ret);
+//		n--;
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+//RAND_MAX
