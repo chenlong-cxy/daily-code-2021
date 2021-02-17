b49e7807539571446cfe3dfd21b3ec5fe2337e24
chenlong_cxy <chenlonghyl@163.com>
2021-02-17 13:55:39 +0800

    daily code

---
+++ test_2021_2_17/test_2021_2_17/test.c
@@ -0,0 +1,77 @@
+//#include<stdio.h>
+//int main()
+//{
+//	double x = 0.0;
+//	double y = 0.0;
+//	for (y = 1.0; y >= -1.0; y -= 0.05)
+//	{
+//		for (x = -1.0; x <= 1.0; x += 0.025)
+//		{
+//			if (x*x + y*y < 1)
+//				printf("*");
+//			else
+//				printf(" ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double x, y;
+//	for (x = 1; x > -1; x -= 0.05)
+//	{
+//		for (y = 1; y>-1; y -= 0.025)
+//		{
+//			putchar(((x*x + y*y) < 1) ? '=' : ' ');
+//		}
+//		printf("\n");
+//	}
+//	getchar();
+//	return (0);
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	double x = 0.0;
+//	double y = 0.0;
+//	for (y = 1.5; y >= -1.5; y -= 0.075)
+//	{
+//		for (x = -1.5; x <= 1.5; x += 0.0375)
+//		{
+//			double tmp = x*x + y*y - 1;
+//			if (tmp*tmp*tmp - x*x*y*y*y < 0)
+//				printf("!");
+//			else
+//				printf(" ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#define PI 3.1415926
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double x = 0.0;
+//	double y = 0.0;
+//	for (y = 13.0; y >= -18.0; y -= 0.775)
+//	{
+//		for (x = -17.0; x <= 17.0; x += 0.425)
+//		{
+//			//
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
