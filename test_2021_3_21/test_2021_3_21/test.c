6bbf953bd93347833bd9c0e73bd83da3fb748af3
chenlong_cxy <chenlonghyl@163.com>
2021-03-21 20:45:25 +0800

    daily code

---
+++ test_2021_3_21/test_2021_3_21/test.c
@@ -0,0 +1,36 @@
+//#include <stdio.h>
+//typedef struct Student
+//{
+//	char name[20];
+//	char sex[5];
+//	int age;
+//	int Class;
+//}Stu;
+//int main()
+//{
+//	Stu s = { "zhangsan", "nan", 20, 2 };
+//	printf("%s %s %d %d\n", s.name, s.sex, s.age, s.Class);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//typedef struct Student
+//{
+//	char name[20];
+//	char sex[5];
+//	int age;
+//	int Class;
+//}Stu;
+//int main()
+//{
+//	Stu arr[3] = { { "zhangsan", "nan", 20, 2 }, { "lisi", "lv", 34, 1 }, { "wangwu", "nan", 54, 3 } };
+//	int i = 0;
+//	printf("%-20s\t%-5s\t%-2s\t%s\n", "姓名", "性别", "年龄", "班级");
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%-20s\t%-5s\t%-2d\t%d\n", arr[i].name, arr[i].sex, arr[i].age, arr[i].Class);
+//	}
+//	return 0;
+//}
+
