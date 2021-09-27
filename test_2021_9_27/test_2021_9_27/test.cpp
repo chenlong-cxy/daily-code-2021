c24f985eb4e6e731feafa347b196ccf48c882d2e
chenlong-xcy <chenlonghyl@163.com>
2021-09-27 16:10:45 +0800

    进程控制草稿

---
+++ test_2021_9_27/test_2021_9_27/test.cpp
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <sys/types.h>
+//int main()
+//{
+//	pid_t id = fork();
+//	if (id == 0){
+//		//child...
+//	}
+//	else if (id > 0){
+//		//father...
+//	}
+//	else{
+//		//error
+//	}
+//	return 0;
+//}
+
+//pid_t fork()
+//{
+//	//1、创建子进程的进程控制块task_struct
+//	//2、创建子进程的进程地址空间mm_struct
+//	//3、创建子进程对应的页表
+//	//其他信息处理...
+//
+//	//OS：将子进程的task_struct链接进入系统的进程列表中
+//	
+//	//子进程创建完毕
+//	
+//	return pid;
+//}
+
+#include <stdio.h>
+int main()
+{
+
+	return 0;
+}
