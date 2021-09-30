7ac07f9a7fe3f5a2093f524638ee41a47f63f895
chenlong-xcy <chenlonghyl@163.com>
2021-09-30 21:49:16 +0800

    进程控制

--- test_2021_9_30/test_2021_9_30/test.cpp
+++ test_2021_9_30/test_2021_9_30/test.cpp
@@ -2,8 +2,15 @@ execl("/usr/bin/ls", "ls", "-a", "-i", "-l", NULL);
 
 execlp("ls", "ls", "-a", "-i", "-l", NULL);
 
+char* myenvp[] = { "MYVAL=2021", NULL };
+execle("./mycmd", "mycmd", NULL, myenvp);
+
 char* myargv[] = { "ls", "-a", "-i", "-l", NULL };
 execv("/usr/bin/ls", myargv);
 
 char* myargv[] = { "ls", "-a", "-i", "-l", NULL };
 execvp("ls", myargv);
+
+char* myargv[] = { "mycmd", NULL };
+char* myenvp[] = { "MYVAL=2021", NULL };
+execve("./mycmd", myargv, myenvp);
