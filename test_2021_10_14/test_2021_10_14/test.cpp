a01d081c30a7f0fd88217c3b898b1a200193e349
chenlong-xcy <chenlonghyl@163.com>
2021-10-14 22:50:06 +0800

    Linux基础IO草稿

--- test_2021_10_14/test_2021_10_14/test.cpp
+++ test_2021_10_14/test_2021_10_14/test.cpp
@@ -16,19 +16,35 @@
 //}
 
 
-#include <stdio.h>
-int main()
-{
-	FILE* fp = fopen("log.txt", "r");
-	if (fp == NULL){
-		perror("fopen");
-		return 1;
-	}
-	char buffer[64];
-	for (int i = 0; i < 5; i++){
-		fgets(buffer, sizeof(buffer), fp);
-		printf("%s", buffer);
-	}
-	fclose(fp);
-	return 0;
-}
+//#include <stdio.h>
+//int main()
+//{
+//	FILE* fp = fopen("log.txt", "r");
+//	if (fp == NULL){
+//		perror("fopen");
+//		return 1;
+//	}
+//	char buffer[64];
+//	for (int i = 0; i < 5; i++){
+//		fgets(buffer, sizeof(buffer), fp);
+//		printf("%s", buffer);
+//	}
+//	fclose(fp);
+//	return 0;
+//}
+
+//extern FILE *stdin;
+//extern FILE *stdout;
+//extern FILE *stderr;
+
+//#include <stdio.h>
+//int main()
+//{
+//	fputs("hello stdin\n", stdout);
+//	fputs("hello stdout\n", stdout);
+//	fputs("hello stderr\n", stdout);
+//	return 0;
+//}
+
+int open(const char *pathname, int flags, mode_t mode);
+O_WRONLY | O_CREAT
